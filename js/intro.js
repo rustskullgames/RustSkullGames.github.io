c93889417abbd3c31c4e6005bd248e2a05f71b94
Rust Skull Games <rustskullgames@gmail.com>
2026-01-15 13:11:34 +0300

    intro.js

--- js/intro.js
+++ js/intro.js
@@ -2,13 +2,16 @@ window.addEventListener("load", () => {
     const intro = document.getElementById("intro");
     const site = document.getElementById("site");
 
-    // 0.9 saniye sonra intro kaybolur
+    // fade-in
+    intro.classList.add("show");
+
+    // fade-out
     setTimeout(() => {
         intro.classList.add("hidden");
         site.classList.add("visible");
     }, 900);
 
-    // 1.3 saniye sonra DOM'dan silinir
+    // tamamen kaldır
     setTimeout(() => {
         intro.style.display = "none";
     }, 1300);
