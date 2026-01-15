18bc7c31747cb6779ee5bde733127354a4a696e1
Rust Skull Games <rustskullgames@gmail.com>
2026-01-15 10:28:52 +0300

    slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,6 @@
+setTimeout(() => {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("site").style.display = "block";
+    document.body.style.overflow = "auto";
+}, 4000);
 
